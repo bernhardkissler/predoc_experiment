7734c6010ad51603ac18afab910ce493da9093d8
bernhard kissler <bernhard.kissler@outlook.de>
2021-05-03 19:34:26 +0200

    implement skipping logic for comprehension questions and round logic for main questions

--- choice_experiment/__init__.py
+++ choice_experiment/__init__.py
@@ -10,7 +10,7 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = "choice_experiment"
     players_per_group = None
-    num_rounds = 1
+    num_rounds = 4
 
 
 class Subsession(BaseSubsession):
@@ -22,6 +22,12 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    understanding_question_1 = models.IntegerField(
+        label="On how many of your choices does your final bonus directly depend?"
+    )
+    understanding_question_2 = models.IntegerField(
+        label="How many options will you be offered during the first 3 rounds?"
+    )
     binary_choice_simple_choose_risky = models.BooleanField(
         choices=[[True, "Option A"], [False, "Option B"],],
         label="What certain amount do you choose?",
@@ -33,25 +39,53 @@ class Player(BasePlayer):
 
 # PAGES
 class WelcomePage(Page):
-    pass
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == 1
 
 
 class InstructionsPage(Page):
-    pass
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == 1
 
 
 class ComprehensionPage(Page):
-    pass
+    form_model = "player"
+    form_fields = ["understanding_question_1", "understanding_question_2"]
+
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == 1
+
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        correct_check = (player.understanding_question_1 == 1) and (
+            player.understanding_question_2 == 2
+        )
+        player.participant.vars["correct_check"] = correct_check
 
 
 class BinaryChoiceSimplePage(Page):
     form_model = "player"
     form_fields = ["binary_choice_simple_choose_risky"]
 
+    @staticmethod
+    def is_displayed(player):
+        return (player.participant.vars["correct_check"]) and (
+            player.round_number in [1, 2, 3]
+        )
+
 
 class BinaryChoiceListPage(Page):
     form_model = "player"
     form_fields = ["binary_choice_list_choose_risky"]
+
+    @staticmethod
+    def is_displayed(player):
+        return (player.participant.vars["correct_check"]) and (
+            player.round_number in [4]
+        )
 
     @staticmethod
     def error_message(player, values):
@@ -60,7 +94,9 @@ class BinaryChoiceListPage(Page):
 
 
 class PayoffPage(Page):
-    pass
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == 4
 
 
 page_sequence = [
